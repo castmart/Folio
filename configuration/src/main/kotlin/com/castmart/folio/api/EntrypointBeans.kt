cb538ac862bd8cdfe735207961e16a62c7462548
Juan Castaneda <jc.castmart@gmail.com>
2024-01-05 19:59:18 +0100

    Added functional endpoints as RouterFunction with the new Regular class handler in details sub-project

--- configuration/src/main/kotlin/com/castmart/folio/api/EntrypointBeans.kt
+++ configuration/src/main/kotlin/com/castmart/folio/api/EntrypointBeans.kt
@@ -4,11 +4,18 @@ import com.castmart.core.usecase.CreateTicketUseCase
 import com.castmart.core.usecase.GetATicketUseCase
 import com.castmart.core.usecase.UpdateTicketUseCase
 import com.castmart.folio.details.entrypoint.TicketRestEntrypointV1
+import com.castmart.folio.details.entrypoint.function.TicketRestFunctionEntrypointV1
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.http.MediaType
+import org.springframework.web.servlet.function.RouterFunction
+import org.springframework.web.servlet.function.ServerResponse
+import org.springframework.web.servlet.function.router
 
 @Configuration
-class EntrypointBeans {
+//@Profile("mvc-annotated")
+class AnnotatedEntrypointBeans {
     @Bean
     fun entrypoint(
         getUseCase: GetATicketUseCase,
@@ -16,5 +23,30 @@ class EntrypointBeans {
         updateTicketUseCase: UpdateTicketUseCase,
     ): TicketRestEntrypointV1 {
         return TicketRestEntrypointV1(createTicketUseCase, updateTicketUseCase, getUseCase)
+    }
+}
+
+@Configuration
+//@Profile("mvc-functional")
+class FunctionalEntrypointBeans {
+
+    @Bean
+    fun functionalHandler(
+        getUseCase: GetATicketUseCase,
+        createTicketUseCase: CreateTicketUseCase,
+        updateTicketUseCase: UpdateTicketUseCase,
+    ): TicketRestFunctionEntrypointV1 {
+        return TicketRestFunctionEntrypointV1(createTicketUseCase, updateTicketUseCase, getUseCase)
+    }
+
+    @Bean
+    fun router(handler: TicketRestFunctionEntrypointV1): RouterFunction<ServerResponse> {
+        return router {
+            GET("/fun/ticket/v1/{ticketId}", handler::getTicketById)
+            accept(MediaType.APPLICATION_JSON).nest {
+                PUT("/fun/ticket/v1", handler::createTicket)
+                POST("/fun/ticket/v1", handler::createTicket)
+            }
+        }
     }
 }
--- details/src/main/kotlin/com/castmart/folio/details/entrypoint/function/TicketRestFunctionEntrypointV1.kt
+++ details/src/main/kotlin/com/castmart/folio/details/entrypoint/function/TicketRestFunctionEntrypointV1.kt
@@ -9,7 +9,6 @@ import org.springframework.web.servlet.function.ServerRequest
 import org.springframework.web.servlet.function.ServerResponse
 import org.springframework.web.servlet.function.ServerResponse.notFound
 import org.springframework.web.servlet.function.ServerResponse.ok
-import org.springframework.web.servlet.function.body
 import java.util.UUID
 
 class TicketRestFunctionEntrypointV1(
